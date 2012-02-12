a72329f4b0fc8900f711290d48cbac8dd931cd72
Njal Karevoll <njal@karevoll.no>
2012-02-12 21:28:58 +0100

    moving git to v0.2.2-dev

--- pykeeper/__init__.py
+++ pykeeper/__init__.py
@@ -4,4 +4,4 @@ from .log_stream import install as install_log_stream, uninstall as uninstall_lo
 
 # See http://www.python.org/dev/peps/pep-0386/ for version numbering, especially NormalizedVersion
 from distutils import version
-version = version.LooseVersion('0.2.1')
+version = version.LooseVersion('0.2.2-dev')
